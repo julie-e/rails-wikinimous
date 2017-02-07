f0a6fb9346dd71260118c71649b10a4754553046
Julie Ehrmann <julie.ehrmann1@gmail.com>
2017-02-07 16:20:37 +0100

    Created my CRUD methods for Wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.destroy_all
+
+10.times do
+  article = Article.create!(
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraph
+    )
+    puts "#{article.title} was created"
+end
